27cc3106f5e21ec67c9abbcc5f44af8cf92a08a5
Samir Djelal <samir.djelal.webdesign@gmail.com>
2022-03-20 23:22:26 +0100

    v0.1.3

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+#[allow(unused_imports)]
 use std::fs::File;
 use std::io::Read;
 use std::process;
@@ -7,6 +8,7 @@ pub fn requirements() {
 		eprintln!("Only ubuntu are supported for now.");
 		process::exit(1);
 	}
+	#[cfg(all(target_os = "linux", any(target_arch = "x86", target_arch = "x86_64")))]
 	if !nix::unistd::Uid::effective().is_root() {
 		eprintln!("You must run this executable with root permissions");
 		process::exit(1);
